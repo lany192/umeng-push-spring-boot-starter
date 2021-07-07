5c38bbf0bf9e7b98df839ecaeff5fe7184e75e61
lany192 <lany192@qq.com>
2021-07-07 21:52:10 +0800

    Update

--- src/main/java/com/github/lany192/push/config/UmengPushAutoConfig.java
+++ src/main/java/com/github/lany192/push/config/UmengPushAutoConfig.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableConfigurationProperties(UmengPushProperties.class)
